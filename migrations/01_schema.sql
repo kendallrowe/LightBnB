96c21ffbcc040f813e3a13625a5b73d3bba06343
Kendall Rowe <kendall.rowe312gmail.com>
2019-07-23 20:06:09 +0000

    Wrote CREATE TABLES for migrations schema

---
+++ migrations/01_schema.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+
+-- Users Table
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+-- Properties Table
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(500) NOT NULL,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night MONEY NOT NULL,
+  parking_spaces SMALLINT NOT NULL,
+  number_of_bathrooms SMALLINT NOT NULL,
+  number_of_bedrooms SMALLINT NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  postal_code VARCHAR(255) NOT NULL,
+  active BOOLEAN NOT NULL
+);
+
+-- Reservations Table
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY,
+  property_id INTEGER NOT NULL REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  start_date TIMESTAMP NOT NULL DEFAULT Now(),
+  end_date TIMESTAMP DEFAULT NULL
+);
+
+-- Property Reviews
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY,
+  guest_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER NOT NULL REFERENCES reservations(id) ON DELETE CASCADE,
+  property_id INTEGER NOT NULL REFERENCES properties(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL,
+  message VARCHAR(500) NOT NULL
+);
