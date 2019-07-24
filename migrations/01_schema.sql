d3a1d6d14edb7d1caadad64b8f71d915c907480c
Kendall Rowe <kendall.rowe312gmail.com>
2019-07-24 19:32:33 +0000

    Imported web abb setup folder

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
 DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+DROP TABLE IF EXISTS rates CASCADE;
+DROP TABLE IF EXISTS guest_reviews CASCADE;
 
 -- Users Table
 CREATE TABLE users (
@@ -18,7 +21,7 @@ CREATE TABLE properties (
   description VARCHAR(90000) NOT NULL,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
-  cost_per_night MONEY NOT NULL,
+  cost_per_night BIGINT NOT NULL,
   parking_spaces SMALLINT NOT NULL,
   number_of_bathrooms SMALLINT NOT NULL,
   number_of_bedrooms SMALLINT NOT NULL,
@@ -45,6 +48,25 @@ CREATE TABLE property_reviews (
   guest_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   reservation_id INTEGER NOT NULL REFERENCES reservations(id) ON DELETE CASCADE,
   property_id INTEGER NOT NULL REFERENCES properties(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL,
+  message VARCHAR(500) NOT NULL
+);
+
+-- Rates Table
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY,
+  start_id TIMESTAMP NOT NULL DEFAULT Now(),
+  end_date TIMESTAMP NOT NULL DEFAULT Now(),
+  cost_per_night BIGINT NOT NULL,
+  property_id INTEGER NOT NULL REFERENCES properties(id) ON DELETE CASCADE
+);
+
+-- Guest Reviews Table
+CREATE TABLE guest_reviews (
+  id SERIAL PRIMARY KEY,
+  guest_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER NOT NULL REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL,
   message VARCHAR(500) NOT NULL
 );
